53d7bcde0ef732e02919d6d70d3a44daf6db5e9b
Andrew Denisenko <nitr0@seti.kr.ua>
2015-01-09 17:44:58 +0200

    dependency changed; version bump

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ripple_api',
-    version='0.0.22',
+    version='0.0.23',
     packages=find_packages(),
     requires=['python (>= 2.7)', 'requests', 'django_model_utils'],
-    install_requires=['requests<2.3.0', 'django-model-utils<1.4.0'],
+    install_requires=['requests<2.3.0', 'django-model-utils'],
     tests_require=['mock'],
     description='Python wrapper for the Ripple API',
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
