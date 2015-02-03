bdd9196298e4972d68d282c4730c98f56007ad85
Andriy Khavryuchenko <akhavr@khavr.com>
2015-02-03 18:58:55 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-ripple_api',
-    version='0.0.26',
+    version='0.0.27',
     packages=find_packages(),
     requires=['python (>= 2.7)', 'requests', 'django_model_utils'],
     install_requires=['requests<2.3.0', 'django-model-utils'],
